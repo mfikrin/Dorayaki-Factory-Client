28441c61dee30b6d1bbaad94aad3e852e6217069
mfikrin <muhammad.fikrin1729@gmail.com>
2021-11-14 14:30:44 +0700

    add initial components n pages

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+
+function Navbar() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Navbar
+
