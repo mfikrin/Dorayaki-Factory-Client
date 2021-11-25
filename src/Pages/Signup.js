01a92c16a66af960541b4c239768a40146a55f84
myahyaibrahim <13519091@std.stei.itb.ac.id>
2021-11-25 08:31:40 +0700

    add koneksi signup page dengan backend

--- src/Pages/Signup.js
+++ src/Pages/Signup.js
@@ -1,50 +1,101 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Form from "react-bootstrap/Form";
 import { Link, useHistory, useLocation } from 'react-router-dom';
 import Button from "react-bootstrap/Button";
 import "./Signup.css";
 import Auth from "../Auth";
+import axios from 'axios';
 
 function Signup() {
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [password2, setPassword2] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
-  const history = useHistory();
-  const location = useLocation();
-  const { from } = location.state || { from: { pathname: '/' } };
+  const [values, setValues] = useState({
+    username: '',
+    email: '',
+    password:'',
+    password2:''
+  });
 
-  function validateForm() {
-    return username.length > 0 && password.length > 0 && email.length>0 && password2.length>0;
-  }
+  const [errors, setErrors]= useState ({});
+  const [isValid, setIsvalid]= useState (true);
+  const [status, setStatus]= useState ();
 
-  const handleSubmit = async event => {
-        event.preventDefault();
-        try{
-          const body = {username,password};
-          await fetch("http://localhost:5000/signup", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(body)
-          }).then(response => response.json().then(data => {
-            if(data.accToken == undefined){
-              setErrorMessage("Invalid Login");
+  const handleChange = e => {
+    const {name, value} = e.target
+    setValues({
+      ...values,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
     
-            }
-            else{
-              localStorage.setItem("user",JSON.stringify(data))
-              // console.log(JSON.stringify(data));
-              history.replace(from);
-              // console.log(Auth.getUser());
-            }
-          }));
-          
+    // Sign up Form validation
+    let errorsTemp = {}
+    setIsvalid(true);
+
+    // Username
+    if(!values.username.trim()){
+        errorsTemp.username = "Username required"
+        setIsvalid(false);
+    }
+    if (!values.email) {
+        errorsTemp.email = 'Email required';
+        setIsvalid(false);
+    } else if (!/\S+@\S+\.\S+/.test(values.email)) {
+        errorsTemp.email = 'Email address is invalid';
+        setIsvalid(false);
+    }
+    if (!values.password) {
+        errorsTemp.password = 'Password is required';
+        setIsvalid(false);
+    }
+    if (!values.password2) {
+        errorsTemp.password2 = 'Password is required';
+        setIsvalid(false);
+    } else if (values.password2 !== values.password) {
+        errorsTemp.password2 = 'Passwords do not match';
+        setIsvalid(false);
+    }
+    
+    setErrors(errorsTemp);
+
+
+    // if (isValid){
+    //   console.log("sudah benar 3")
+    //   const body = {
+    //     username: values.username,
+    //     email: values.email,
+    //     password: values.password
+    //   }
+    //   axios.post("http://localhost:5000/users", body).then(response =>{
+    //     console.log(response.data)
+    //     if (response.data.username==values.username && response.data.email==values.email && response.data.password==values.password){
+    //       statusTemp = "Sign up berhasil"
+    //     }
+    //   })
+    // }
+    // setStatus(statusTemp)
+  };
+
+  useEffect(()=>{
+    let statusTemp = '';
+    if(isValid){
+      const body = {
+      username: values.username,
+      email: values.email,
+      password: values.password
+      }
+      axios.post("http://localhost:5000/users", body).then(response =>{
+        if (response.data.username==values.username && response.data.email==values.email && response.data.password==values.password){
+          statusTemp = "Sign up berhasil";
+          setStatus(statusTemp);
+        } else {
+          statusTemp = '';
+          setStatus(statusTemp);
         }
-        catch(err){
-          console.error(err.message);
-        }
-};
+      });
+    }
+  }, [errors]);
 
   return (
     <div className ="Body">
@@ -55,40 +106,55 @@ function Signup() {
           <Form.Label>Username</Form.Label>
           <Form.Control
             autoFocus
-            type="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
+            // id='username'
+            type="text"
+            name='username'
+            value={values.username}
+            onChange={handleChange}
           />
         </Form.Group>
+        {errors.username && <p className='error-prompt'>{errors.username}</p>}
         <Form.Group size="lg" controlId="email">
           <Form.Label>Email</Form.Label>
           <Form.Control
             autoFocus
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            // id='email'
+            type="text"
+            name='email'
+            value={values.email}
+            onChange={handleChange}
           />
         </Form.Group>
+        {errors.email && <p className='error-prompt'>{errors.email}</p>}
         <Form.Group size="lg" controlId="password">
           <Form.Label>Password</Form.Label>
           <Form.Control
+            // id='password'
             type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            name='password'
+            value={values.password}
+            onChange={handleChange}
           />
         </Form.Group>
+        {errors.password && <p className='error-prompt'>{errors.password}</p>}
         <Form.Group size="lg" controlId="password2">
           <Form.Label>Password</Form.Label>
           <Form.Control
+            // id='password2'
             type="password"
-            value={password2}
-            onChange={(e) => setPassword2(e.target.value)}
+            name='password2'
+            value={values.password2}
+            onChange={handleChange}
           />
         </Form.Group>
-        <Button block size="lg" type="submit" disabled={!validateForm()}>
+        {errors.password2 && <p className='error-prompt'>{errors.password2}</p>}
+        <Button block size="lg" type="submit" >
           Sign Up
         </Button>
-        {errorMessage && <div className="error"> {errorMessage} </div>}
+        <span className='form-input-login'>
+            Already have an account? Login <a href='/login'>here</a>
+        </span>
+        {status && <p className='status-prompt'>{status}</p>}
       </Form>
     </div>
     </div>
